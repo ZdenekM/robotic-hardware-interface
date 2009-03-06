f977446b93dcf79e73a5c76c36cad2f20f7f8a7a
zdenek.materna <zdenek.materna@227f52c0-8a28-11dd-acfe-89937ea44748>
2009-03-06 10:01:23 +0000

    základní funkčnost

---
+++ elektronika/SensMod/SensMod.h
@@ -0,0 +1,28 @@
+#ifndef SENS_H
+#define SENS_H
+
+// typ pro stav modulu
+typedef enum {S_FAST_SCAN, S_FULL_SCAN, S_ERROR} ts_state;
+
+typedef struct {
+
+	// pole pro uložení zmìøených vzdáleností ze Sharpù
+	volatile uint16_t sharp[4];
+
+	// stav modulu
+	volatile ts_state s_state;
+
+	// pole pro uložení full scan dat z ultrazvuku
+	volatile uint16_t us_full[5];
+
+	// prom. pro uložení rychlého scanování (za jízdy)
+	volatile uint16_t us_fast;
+
+	// údaje z kompasu
+	volatile uint16_t comp;
+
+
+} tmod_state;
+
+
+#endif
